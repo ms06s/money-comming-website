a4c8f086669eeb46277d9cfe8de533055d2d9d8c
ms06s <uselesspassion@outlook.com>
2020-08-12 15:49:45 +0800

    deploy

---
+++ precache-manifest.bc07ff0782d8bc38613ee264f599f1b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c7f852a94f864fa4bb",
+    "url": "/money-comming-website/css/app.23772591.css"
+  },
+  {
+    "revision": "13ce0d305abe6f7e5cdfce0ea1b5f6b4",
+    "url": "/money-comming-website/index.html"
+  },
+  {
+    "revision": "e4c7f852a94f864fa4bb",
+    "url": "/money-comming-website/js/app.104a6ec1.js"
+  },
+  {
+    "revision": "5b1c6006466318a1c8f8",
+    "url": "/money-comming-website/js/chunk-vendors.46761cf7.js"
+  },
+  {
+    "revision": "e09939e8bdb3bc07e427e0d8dbf41c91",
+    "url": "/money-comming-website/manifest.json"
+  },
+  {
+    "revision": "afe80a2e60a24663d414b85e8963b54e",
+    "url": "/money-comming-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-comming-website/robots.txt"
+  }
+]);
